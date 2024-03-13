d6422b2b552c8a0e0238f241186170027c1c8d5e
Laura <laura.daguerds@gmail.com>
2024-03-13 15:25:17 -0300

    Exercicios 1, 2 e 3

---
+++ exercicios-intro-python/02-exercicio.py
@@ -0,0 +1,10 @@
+# Escreva um programa em Python que solicita ao usuário 
+# três números e apresenta a média aritmética dos números informados.
+
+lista = [
+    float(input('Entre com primeiro número: ')),
+    float(input('Entre com segundo número: ')),
+    float(input('Entre com terceiro número: '))
+]
+
+print(f'Média = {sum(lista) / len(lista)}')
